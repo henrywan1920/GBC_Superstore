7cabf1fca777fdf6247f1019db0e272ba576ab3f
henrywan1920 <65926241+henrywan1920@users.noreply.github.com>
2022-02-15 09:25:49 -0500

    Added selecting statements to tables

--- query_10_records.sql
+++ query_10_records.sql
@@ -5,3 +5,23 @@ LIMIT 10;
 /* 10 complete records from customers */
 SELECT * FROM customers
 LIMIT 10;
+
+/* 10 complete records from orders */
+SELECT * FROM orders
+LIMIT 10;
+
+/* 10 complete records from people */
+SELECT * FROM people
+LIMIT 10;
+
+/* 10 complete records from products */
+SELECT * FROM products
+LIMIT 10;
+
+/* 10 complete records from returns */
+SELECT * FROM returns
+LIMIT 10;
+
+/* 10 complete records from shipments */
+SELECT * FROM shipments
+LIMIT 10;
